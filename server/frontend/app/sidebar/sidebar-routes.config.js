83ad0b9986f8fabb3d684900878cecbff0404c7b
Marc Vila <marcvilagomez@gmail.com>
2017-04-22 16:55:40 +0200

    Removing unused components

--- server/frontend/app/sidebar/sidebar-routes.config.js
+++ server/frontend/app/sidebar/sidebar-routes.config.js
@@ -2,11 +2,7 @@
 exports.ROUTES = [
     { path: 'dashboard', title: 'Dashboard', icon: 'pe-7s-graph', class: '' },
     { path: 'user', title: 'User Profile', icon: 'pe-7s-user', class: '' },
-    { path: 'table', title: 'Table List', icon: 'pe-7s-note2', class: '' },
-    { path: 'typography', title: 'Typography', icon: 'pe-7s-news-paper', class: '' },
-    { path: 'icons', title: 'Icons', icon: 'pe-7s-science', class: '' },
-    { path: 'maps', title: 'Maps', icon: 'pe-7s-map-marker', class: '' },
-    { path: 'notifications', title: 'Notifications', icon: 'pe-7s-bell', class: '' },
-    { path: 'upgrade', title: 'Upgrade to PRO', icon: 'pe-7s-rocket', class: 'active-pro' },
+    { path: 'table', title: 'Check Information', icon: 'pe-7s-note2', class: '' },
+    { path: 'maps', title: 'Check Location', icon: 'pe-7s-map-marker', class: '' },
 ];
 //# sourceMappingURL=sidebar-routes.config.js.map
